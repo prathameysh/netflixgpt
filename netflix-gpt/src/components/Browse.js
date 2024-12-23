13235d9573f6512dd73605002668505e13f2bb1c
prathameysh <prathameshgavatre@gmail.com>
2024-12-23 10:58:28 +0530

    sign/sing up form && Login/header

---
+++ netflix-gpt/src/components/Browse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Browse() {
+  return (
+    <div>
+      B
+    </div>
+  )
+}
+
+export default Browse
---
+++ netflix-gpt/src/components/Ish.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Ish = () => {
+  return (
+    <div>
+        <img src="https://i.ibb.co/VQFHVF1/20240717-151119.jpg" alt="20240717-151119" border="0"/>
+        <h1>Hi my name is Istwari</h1>
+
+    </div>
+  )
+}
+
+export default Ish
