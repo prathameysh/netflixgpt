5d7d7be858c652e01cccc960e2f5c52236fe6996
prathameysh <prathameshgavatre@gmail.com>
2024-12-31 22:14:51 +0530

    TMDB API data Fetch && built MainContainer

--- netflix-gpt/src/utils/appStore.js
+++ netflix-gpt/src/utils/appStore.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import movieReducer from "./moviesSlice";
+
 const appStore = configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
+  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools in development mode only
 });
+
 export default appStore;
